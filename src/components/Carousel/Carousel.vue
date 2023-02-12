24a6bf86191b3f80600e42861231e202dc4d6d05
L1angCt <1044588442@qq.com>
2023-02-12 17:27:33 +0800

    banner图

--- src/components/Carousel/Carousel.vue
+++ src/components/Carousel/Carousel.vue
@@ -1,12 +1,27 @@
 <template>
-  
+    <swiper class="carousel" :style="{ height: height }" autoplay indicator-dots circular>
+        <swiper-item v-for="item in banners" :key="item.id">
+            <image :src="item.imgUrl"></image>
+        </swiper-item>
+    </swiper>
 </template>
 
 <script>
 export default {
+    props: ["banners", "height"],
+    data() {
+        return {
+
+        }
+    },
+    mounted() {
+    },
+
 };
 </script>
 
 <style scoped>
-
+.carousel {
+    height: 100%;
+}
 </style>
