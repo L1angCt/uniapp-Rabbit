bb648cd5c78cad20dcda3961223ffc5172667dcd
L1angCt <1044588442@qq.com>
2023-02-12 11:52:09 +0800

    Vuex测试

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,22 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import persistence from 'vuex-uniapp-persistence'
 
-Vue.use(Vuex);
+Vue.use(Vuex);//vue的插件机制
+// Vuex.store构造器选项
 const store = new Vuex.Store({
+  plugins: [persistence()],
   state: {
+    count: 100,
+    obj: { name: '李华', age: 20 }
   },
   getters: {},
-  mutations: {},
-  modules: { },
+  mutations: {
+    addCount(state, payload) {
+      state.count += payload
+      // console.log(123);
+    }
+  },
+  modules: {},
 });
 export default store;
